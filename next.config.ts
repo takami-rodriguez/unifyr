0888ee98f7f9ab30bba66cfc6dbdfce547e48b35
Brian Carbone <22246091-briancarbone@users.noreply.gitlab.com>
2024-12-10 17:25:23 -0500

    disable image optimization for now

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,9 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: "export",
   trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
   experimental: {
     turbo: {
       // ...
