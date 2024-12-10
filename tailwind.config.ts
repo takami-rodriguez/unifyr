837441d92e981ed005033e009d67c841fdb487f1
phil <phil@poppins.agency>
2024-12-10 21:39:21 +0000

    Add back robots

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  darkMode: ["class", "class"],
+  darkMode: ["class"],
   content: [
     "app/**/*.{html,md,mdx,ts,tsx}",
     "components/**/*.{html,md,mdx,ts,tsx}"
