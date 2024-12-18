d5f19d5694b225a7c551b0892b25572f61505dee
Brian Carbone <22246091-briancarbone@users.noreply.gitlab.com>
2024-12-17 23:20:59 -0500

    set assetPrefix based on phase

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,28 @@
 import type { NextConfig } from "next";
-
+import { PHASE_DEVELOPMENT_SERVER } from "next/constants";
 import withBundleAnalyzer from "@next/bundle-analyzer";
 
-const bundleAnalyzer = withBundleAnalyzer({
-  enabled: process.env.ANALYZE === "true",
-});
+export default (phase: string) => {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER;
 
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  output: "export",
-  trailingSlash: true,
-  assetPrefix: process.env.URL!,
-  images: {
-    unoptimized: true,
-  },
-  experimental: {
-    turbo: {
-      // ...
+  const nextConfig: NextConfig = {
+    reactStrictMode: true,
+    output: "export",
+    trailingSlash: true,
+    assetPrefix: isDev ? undefined : process.env.URL!,
+    images: {
+      unoptimized: true,
     },
-  },
-};
+    experimental: {
+      turbo: {
+        // ...
+      },
+    },
+  };
+  
+  const bundleAnalyzer = withBundleAnalyzer({
+    enabled: process.env.ANALYZE === "true",
+  });
 
-export default bundleAnalyzer(nextConfig);
+  return bundleAnalyzer(nextConfig);
+}
