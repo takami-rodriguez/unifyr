229aecf324aedf76fda16868ec0b6eab4162f792
Brian Carbone <22246091-briancarbone@users.noreply.gitlab.com>
2024-12-18 09:12:02 -0500

    revert assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,12 @@ import { PHASE_DEVELOPMENT_SERVER } from "next/constants";
 import withBundleAnalyzer from "@next/bundle-analyzer";
 
 export default (phase: string) => {
-  const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+  // const isDev = phase === PHASE_DEVELOPMENT_SERVER;
 
   const nextConfig: NextConfig = {
     reactStrictMode: true,
     output: "export",
     trailingSlash: true,
-    assetPrefix: isDev ? undefined : process.env.URL!,
     images: {
       unoptimized: true,
     },
