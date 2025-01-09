01ff0d950a499734d3896143b2b71bba0ecdfe24
phil <phil@poppins.agency>
2025-01-09 13:53:13 +0000

    Remove bg gradient for now

--- src/app/get-a-demo/page.tsx
+++ src/app/get-a-demo/page.tsx
@@ -7,10 +7,7 @@ const GetADemo = async () => {
   return (
     <div
       className="max-w-5xl mx-auto py-12"
-      style={{
-        background:
-          "radial-gradient(461.91% 160.49% at 17.47% -33.36%, rgba(215, 14, 134, 0.04) 0%, rgba(36, 56, 139, 0.05) 99.68%);",
-      }}
+     
     >
       <div className="grid grid-cols-1 lg:grid-cols-7 items-center gap-16">
         <div className="space-y-8 col-span-4">
