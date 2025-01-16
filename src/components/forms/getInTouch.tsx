8ede45d77ea0351bf1cac727a3df385e25bd225f
phil <phil@poppins.agency>
2025-01-16 20:09:16 +0000

    IP - cloudflare

--- src/components/forms/getInTouch.tsx
+++ src/components/forms/getInTouch.tsx
@@ -10,16 +10,14 @@ import {
   SelectTrigger,
   SelectValue,
 } from "./components/select";
-import { FormEvent, useRef, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { Turnstile } from "next-turnstile";
 import { cn } from "@/lib/utils";
 
 const GetInTouch = ({ id }: { id: string }) => {
-  const formRef = useRef<HTMLFormElement>(null);
-
   const [success, setSuccess] = useState(false);
-  const [turnstileStatus, setTurnstileStatus] = useState("required");
-  const [turnstileError, setTurnstileError] = useState<string | null>(null);
+  // const [turnstileStatus, setTurnstileStatus] = useState("required");
+  // const [turnstileError, setTurnstileError] = useState<string | null>(null);
   const [errors, setErrors] = useState({
     first_name: undefined,
     last_name: undefined,
@@ -33,22 +31,19 @@ const GetInTouch = ({ id }: { id: string }) => {
       form.elements,
       form.elements.namedItem("cf-turnstile-response")
     );
-    if (!formRef.current) return;
-    const formData = new FormData(formRef.current);
-    const token = formData.get("cf-turnstile-response");
+
     const first_name = (
-      form.elements.namedItem("first_name") as HTMLInputElement
+      form.elements.namedItem("FirstName") as HTMLInputElement
     ).value;
-    const last_name = (form.elements.namedItem("last_name") as HTMLInputElement)
+    const last_name = (form.elements.namedItem("LastName") as HTMLInputElement)
       .value;
-    // const token = (
-    //   form.elements.namedItem("cf-turnstile-response") as HTMLInputElement
-    // ).value;
+    const token = (
+      form.elements.namedItem("cf-turnstile-response") as HTMLInputElement
+    ).value;
     const entity_type = (
       form.elements.namedItem("entity_type__c") as HTMLInputElement
     ).value;
-    const email = (form.elements.namedItem("email") as HTMLInputElement).value;
-    console.log(token);
+    const email = (form.elements.namedItem("Email") as HTMLInputElement).value;
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
     myHeaders.append("Access-Control-Allow-Origin", "*");
@@ -58,7 +53,6 @@ const GetInTouch = ({ id }: { id: string }) => {
     urlencoded.append("cf-turnstile-response", String(token));
     urlencoded.append("FirstName", first_name);
     urlencoded.append("LastName", last_name);
-
     urlencoded.append("entity_type__c", entity_type);
 
     const requestOptions: RequestInit = {
@@ -79,7 +73,25 @@ const GetInTouch = ({ id }: { id: string }) => {
         setErrors({ ...errors, marketo: error });
       });
   };
+
   console.log({ errors });
+
+  useEffect(() => {
+    // if (window)
+    // window.onloadTurnstileCallback = function () {
+    //   turnstile.render(".cf-turnstile", {
+    //     sitekey: "0x4AAAAAAA5VmWokYJQQgCCK",
+    //     callback: function (token) {
+    //       console.log(`Challenge Success ${token}`);
+    //     },
+    //   });
+    // };
+  }, []);
+
+  const handleVerify = (token: string) => {
+    // Handle the verification token
+    console.log("Verification successful:", token);
+  };
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log("submitting form");
@@ -95,94 +107,67 @@ const GetInTouch = ({ id }: { id: string }) => {
   };
   return (
     <div className="w-full">
-      <form id={id} ref={formRef} onSubmit={handleSubmit}>
+      <form id={id} onSubmit={handleSubmit}>
         <div
-          className={cn("bg-white/60 rounded-2xl py-10 px-14 space-y-6", {
-            "h-[434px]": turnstileStatus === "required",
-          })}
+          className={cn("bg-white/60 rounded-2xl py-10 px-14 space-y-6")}
           style={boxShadow}
         >
-          <Turnstile
-            siteKey={process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY!}
-            retry="auto"
-            refreshExpired="auto"
-            sandbox={process.env.NODE_ENV === "development"}
-            onError={() => {
-              setTurnstileStatus("error");
-              setTurnstileError(
-                "Security check failed. Please refresh and try again."
-              );
-            }}
-            onExpire={() => {
-              setTurnstileStatus("expired");
-              setTurnstileError(
-                "Security check expired. Please refresh and verify again."
-              );
-            }}
-            onLoad={() => {
-              setTurnstileStatus("required");
-              setTurnstileError(null);
-            }}
-            onVerify={() => {
-              setTurnstileStatus("success");
-              setTurnstileError(null);
-            }}
-          />
-          {turnstileStatus === "success" ? (
-            <>
-              <div className="space-y-1">
-                <Label>I am a...</Label>
-                <Select name="entity_type__c">
-                  <SelectTrigger className="w-full">
-                    <SelectValue placeholder="Please select" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="supplier">Supplier</SelectItem>
-                    <SelectItem value="agency">Agency</SelectItem>
-                    <SelectItem value="partner">Partner</SelectItem>
-                  </SelectContent>
-                </Select>
-              </div>
-              <div className="grid grid-cols-2 gap-6">
-                <InputField
-                  type="text"
-                  label="First Name"
-                  name="FirstName"
-                  placeholder="Alex"
-                  required
-                  error={errors.first_name}
-                />
-                <InputField
-                  type="text"
-                  label="Last Name"
-                  name="LastName"
-                  placeholder="Bloggs"
-                  required
-                  error={errors.last_name}
-                />
-              </div>
+          <>
+            <div className="space-y-1">
+              <Label>I am a...</Label>
+              <Select name="entity_type__c">
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Please select" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="supplier">Supplier</SelectItem>
+                  <SelectItem value="agency">Agency</SelectItem>
+                  <SelectItem value="partner">Partner</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+            <div className="grid grid-cols-2 gap-6">
               <InputField
-                label="Email address"
-                type="email"
-                name="Email"
-                placeholder="alex.bloggs@email.com"
+                type="text"
+                label="First Name"
+                name="FirstName"
+                placeholder="Alex"
                 required
-                error={errors.email}
+                error={errors.first_name}
               />
-              <div className="flex justify-between">
-                <div>{errors.marketo}</div>
-                <Button variant={"primary"} type="submit" disabled={success}>
-                  {success ? "Success" : "Submit"}
-                </Button>
-              </div>
-            </>
-          ) : (
-            <>
-              {turnstileError && (
-                <div className="text-red-500 text-sm">{turnstileError}</div>
-              )}
-            </>
-          )}
+              <InputField
+                type="text"
+                label="Last Name"
+                name="LastName"
+                placeholder="Bloggs"
+                required
+                error={errors.last_name}
+              />
+            </div>
+            <InputField
+              label="Email address"
+              type="email"
+              name="Email"
+              placeholder="alex.bloggs@email.com"
+              required
+              error={errors.email}
+            />
+            <div className="flex justify-between">
+              <div>{errors.marketo}</div>
+              <Button variant={"primary"} type="submit" disabled={success}>
+                {success ? "Success" : "Submit"}
+              </Button>
+            </div>
+          </>
+
+        
+          <Turnstile
+            className="hidden"
+            siteKey="0x4AAAAAAA5VmWokYJQQgCCK"
+            onVerify={handleVerify}
+            theme="light"
+            appearance="interaction-only"
+          />
         </div>
       </form>
     </div>
