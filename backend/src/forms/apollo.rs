433d094a36ff509a83dad0693d41c2f84f68ab09
Brian Carbone <22246091-briancarbone@users.noreply.gitlab.com>
2025-01-17 14:53:24 -0500

    fix apollo enrichment

--- backend/src/forms/apollo.rs
+++ backend/src/forms/apollo.rs
@@ -1,13 +1,17 @@
 use super::FormDataMap;
-use crate::error::EdgeError;
+use crate::{error::EdgeError, forms::creds::CREDENTIALS};
 use fastly::Request;
 use url::Url;
 
 const APOLLO_BACKEND: &str = "apollo";
 const APOLLO_ENRICHMENT: &str = "https://api.apollo.io/api/v1/people/match";
 
+// hard-coded marketo built-in keys
 pub fn enrich(formdata: &mut FormDataMap) -> Result<(), EdgeError> {
-    let mut vec = vec![];
+    let mut vec = vec![
+        ("reveal_personal_emails", "false"),
+        ("reveal_phone_number", "false"),
+    ];
 
     let email = formdata
         .get("email")
@@ -24,26 +28,40 @@ pub fn enrich(formdata: &mut FormDataMap) -> Result<(), EdgeError> {
     }
 
     let url = Url::parse_with_params(APOLLO_ENRICHMENT, vec)?;
-    let mut response = Request::post(url).send(APOLLO_BACKEND)?;
-    let value: apollo_enrichment::Response = response.take_body_json()?;
+    let mut response = Request::post(url)
+        .with_header("x-api-key", &CREDENTIALS.apollo_match_key)
+        .send(APOLLO_BACKEND)?;
+
+    let value = response
+        .take_body_json::<apollo_enrichment::Response>()?
+        .person;
 
     formdata.insert("title".into(), value.title);
     formdata.insert("company".into(), value.organization.name);
-    formdata.insert("Apollo_Primary_Industry__c".into(), value.organization.industry);
+    formdata.insert(
+        "Apollo_Primary_Industry__c".into(),
+        value.organization.industry,
+    );
 
     Ok(())
 }
 
+#[allow(dead_code)]
 mod apollo_enrichment {
     use serde::Deserialize;
 
-    #[derive(Deserialize)]
+    #[derive(Debug, Deserialize)]
     pub struct Response {
+        pub person: Person,
+    }
+
+    #[derive(Debug, Deserialize)]
+    pub struct Person {
         pub title: String,
         pub organization: Organization,
     }
 
-    #[derive(Deserialize)]
+    #[derive(Debug, Deserialize)]
     pub struct Organization {
         pub name: String,
         pub industry: String,
