d05af2191b94351436f668411fa6469aed8d62b6
Brian Carbone <160078386+briancarbone@users.noreply.github.com>
2025-02-11 12:49:26 -0500

    reduce blog line height

--- src/components/markdown.tsx
+++ src/components/markdown.tsx
@@ -24,7 +24,7 @@ const Markdown: React.FC<MarkdownProps> = async ({
         "prose-code:text-wrap",
         "prose-h2:pt-8 prose-h2:text-[2rem] prose-h2:leading-[2.2rem] md:prose-h2:text-[2.5rem] md:prose-h2:leading-[2.5rem]",
         "prose-h3:pt-8 prose-h3:text-[1.75rem] prose-h3:leading-[1.75rem] md:prose-h3:text-[2rem] md:prose-h3:leading-[2rem]",
-        "prose-p:font-resources prose-p:text-[1.125rem] prose-p:leading-[1.75rem] md:prose-p:text-[1.25rem] md:prose-p:leading-[2.5rem]",
+        "prose-p:font-resources prose-p:text-[1.125rem] prose-p:leading-[1.75rem] md:prose-p:text-[1.25rem] md:prose-p:leading-[2rem]",
       )}
       dangerouslySetInnerHTML={{ __html: md.render(content) }}
     ></div>
