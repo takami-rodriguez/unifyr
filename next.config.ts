3f3f2b7492bfb64d07960d6459c0b9cf934a602f
Brian Carbone <160078386+briancarbone@users.noreply.github.com>
2025-02-13 11:57:52 -0500

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,13 @@
 import type { NextConfig } from "next";
 import withBundleAnalyzer from "@next/bundle-analyzer";
 
-const config = (_: string) => {
-  // const isDev = phase === PHASE_DEVELOPMENT_SERVER;
-
+const config = () => {
   const nextConfig: NextConfig = {
     reactStrictMode: true,
     output: "export",
     trailingSlash: true,
     images: {
       unoptimized: true,
-    },
-    experimental: {
-      turbo: {
-        // ...
-      },
     },
     eslint: {
       ignoreDuringBuilds: true,
