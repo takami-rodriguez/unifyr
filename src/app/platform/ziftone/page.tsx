8f6eb9d85898a693ca6fdca429979a7531fc11c1
Brian Carbone <160078386+briancarbone@users.noreply.github.com>
2025-03-06 12:29:21 -0500

    add 'e' parameter for form entity type

--- src/app/platform/ziftone/page.tsx
+++ src/app/platform/ziftone/page.tsx
@@ -9,17 +9,17 @@ const meta = {
   title: "ZiftONE Platform",
   description:
     "ZiftONE is the only platform that combines the power of channel marketing and sales automation to deliver a single, integrated solution for channel organizations.",
-}
+};
 
 export async function generateMetadata(
-  { }: PageProps,
+  {}: PageProps,
   parent: ResolvingMetadata,
 ): Promise<Metadata> {
   return getDynamicPageSEOData(meta, parent);
 }
 
 const ZiftOnePlatformPage = async () => {
-  return ( <PlatformTemplate data={data} /> );
+  return <PlatformTemplate data={data} />;
 };
 
 export default ZiftOnePlatformPage;
