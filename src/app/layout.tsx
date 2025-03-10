20bed9f404cc6350addc9b1f44898ecd117bc589
Brian Carbone <160078386+briancarbone@users.noreply.github.com>
2025-03-10 10:50:58 -0400

    release promo banner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,11 +27,12 @@ export default function RootLayout({
         className={`${epicene.variable} ${epiceneText.variable} ${sohne.variable} font-sans`}
       >
         <Notice>
-        {/*<Link href="/lp/promo/" className="underline underline-offset-2">
+          <Link href="/lp/promo/" className="underline underline-offset-2">
             <span className="font-bold">Time-limited offer: </span>Upgrade from
-            your existing PRM and save at least 20% off your current contract.
-          </Link>*/}
-         Zift Solutions is now Unifyr.
+            your existing PRM and save{" "}
+            <span className="italic">at least 20%</span> off your current
+            contract.
+          </Link>
         </Notice>
         <ClientLayout>
           <div className="px-4 md:px-5">
--- src/components/notice.tsx
+++ src/components/notice.tsx
@@ -2,7 +2,7 @@ import React, { PropsWithChildren } from "react";
 
 const Notice: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="w-screen bg-secondary">
+    <div className="w-screen bg-green-700">
       <div className="mx-auto size-fit px-4 py-2 text-center leading-[1.25] text-white">
         {children}
       </div>
