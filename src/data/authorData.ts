b821d1afee7ef8105896ea87c66c542ed9eb3fde
Brian Carbone <brian@briancarbone.com>
2025-03-24 11:39:33 -0400

    use blog for businesswire pr post, for now

--- src/data/authorData.ts
+++ src/data/authorData.ts
@@ -6,8 +6,17 @@ export const authorData: Record<string, AuthorDetails> = {
     name: "Brian Carbone",
     image: {
       url: "/images/bchs-circ.webp",
-      alt: "Author",
+      alt: "Brian Carbone",
       id: "1-author-image",
+    },
+  },
+  businesswire: {
+    _uid: "0B388F26-5707-4E39-AAD4-7872D9585708",
+    name: "Business Wire",
+    image: {
+      url: "/images/businesswire.webp",
+      alt: "Business Wire",
+      id: "2-author-image",
     },
   },
 };
