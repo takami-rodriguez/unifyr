09f523043de4979a8fcc5cb58b611ae4a4297a62
Brian Carbone <brian@briancarbone.com>
2025-04-22 09:47:03 -0400

    fix build

--- src/components/landing/types.ts
+++ src/components/landing/types.ts
@@ -1,10 +1,8 @@
-import { ReactNode } from "react";
-
 // TODO: Move to types once it's mature enough
 
 export type BlockData = {
-  description: ReactNode;
-  title: ReactNode;
+  description: string;
+  title: string;
 };
 
 export type BlockDataWithIcon = BlockData & {
