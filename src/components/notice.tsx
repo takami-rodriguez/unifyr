66b84abbaafafa6fa57924b8f1755b99f8048251
Brian Carbone <brian@briancarbone.com>
2025-04-22 13:06:16 -0400

    restore banner

--- src/components/notice.tsx
+++ src/components/notice.tsx
@@ -2,8 +2,8 @@ import React, { PropsWithChildren } from "react";
 
 const Notice: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="w-screen bg-green-700">
-      <div className="mx-auto size-fit px-4 py-2 text-center leading-[1.25] text-white">
+    <div className="w-screen bg-slate-300">
+      <div className="mx-auto size-fit px-4 py-2 text-center font-bold leading-[1.25] text-black">
         {children}
       </div>
     </div>
